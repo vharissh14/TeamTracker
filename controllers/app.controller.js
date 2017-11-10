dfe2b116cdeb36d5a28333b9787c14719cf018ab
astro-5 <astro@elpisitsolutions.com>
2017-11-10 12:53:49 +0530

    login redirection issue fixed

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -4,17 +4,17 @@ var router = express.Router();
 
 router.use('/', function (req, res, next) {
     if (req.path !== '/login' && !req.session.token) {
-        return res.redirect('/login?returnUrl=' + encodeURIComponent('/app' + req.path));
+        return res.redirect('/login?');
     }
  
     next();
 });
+router.use('/', express.static('app'));
 // make JWT token available to angular app
 router.get('/token', function (req, res) {
     res.send(req.session);
 });
 
 // serve angular app files from the '/app' route
-router.use('/', express.static('app'));
 
 module.exports = router;
